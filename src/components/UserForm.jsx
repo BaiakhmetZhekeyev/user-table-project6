d668b05a0fa9fe40e46f7f84a1fd55fe7ab4355b
Baiakhmet <baiakhmet.zhekeyev@gmail.com>
2023-08-18 18:52:41 +0600

    validation

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "./UserForm.module.css";
 import { useFormik } from "formik";
 import { Button } from "@mui/material";
+import * as Yup from "yup";
 
 const UserForm = ({ setIsModalActive, addUser, tableLenght }) => {
   const [image, setImage] = React.useState(null);
@@ -13,6 +14,21 @@ const UserForm = ({ setIsModalActive, addUser, tableLenght }) => {
       age: "",
       city: "",
     },
+    validationSchema: Yup.object({
+      firstName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Reaquired"),
+      lastName: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Reaquired"),
+      age: Yup.number("It must be number")
+        .min(16)
+        .max(100)
+        .required("Reaquired"),
+      city: Yup.string()
+        .max(15, "Must be 15 characters or less")
+        .required("Reaquired"),
+    }),
     onSubmit: (values) => {
       const userRating = tableLenght + 1;
       let userId = Date.now().toString();
@@ -30,7 +46,11 @@ const UserForm = ({ setIsModalActive, addUser, tableLenght }) => {
               name="firstName"
               onChange={formik.handleChange}
               value={formik.values.firstName}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.firstName && formik.errors.firstName ? (
+              <p style={{ color: "red" }}>{formik.errors.firstName}</p>
+            ) : null}
           </div>
           <div className={styled.formGroup}>
             <label htmlFor="lastName">LastName</label>
@@ -38,7 +58,11 @@ const UserForm = ({ setIsModalActive, addUser, tableLenght }) => {
               name="lastName"
               onChange={formik.handleChange}
               value={formik.values.lastName}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.lastName && formik.errors.lastName ? (
+              <p style={{ color: "red" }}>{formik.errors.lastName}</p>
+            ) : null}
           </div>
           <div className={styled.formGroup}>
             <label htmlFor="age">Age</label>
@@ -46,7 +70,11 @@ const UserForm = ({ setIsModalActive, addUser, tableLenght }) => {
               name="age"
               onChange={formik.handleChange}
               value={formik.values.age}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.age && formik.errors.age ? (
+              <p style={{ color: "red" }}>{formik.errors.age}</p>
+            ) : null}
           </div>
           <div className={styled.formGroup}>
             <label htmlFor="city">City</label>
@@ -54,7 +82,11 @@ const UserForm = ({ setIsModalActive, addUser, tableLenght }) => {
               name="city"
               onChange={formik.handleChange}
               value={formik.values.city}
+              onBlur={formik.handleBlur}
             />
+            {formik.touched.city && formik.errors.city ? (
+              <p style={{ color: "red" }}>{formik.errors.city}</p>
+            ) : null}
           </div>
           <div className={styled.formGroup}>
             <label htmlFor="avatar">Avatar</label>
