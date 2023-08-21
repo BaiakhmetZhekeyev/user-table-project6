f6e6f112de0c8ebb1fa9e0d9c3fac050c817ccc7
Baiakhmet <baiakhmet.zhekeyev@gmail.com>
2023-08-21 08:58:57 +0600

    display form using formik's 'Form' component

--- src/components/Users/UserForm.jsx
+++ src/components/Users/UserForm.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "./UserForm.module.css";
-import { useFormik } from "formik";
+import { Formik, Form, ErrorMessage, Field } from "formik";
 import { Avatar, Button } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import MyUpload from "../UI/MyUpload/MyUpload";
@@ -14,6 +14,13 @@ const defaultValue = {
   city: "",
 };
 
+const userInfo = [
+  { name: "firstName", label: "FirstName", id: 1 },
+  { name: "lastName", label: "LastName", id: 2 },
+  { name: "age", label: "Age", id: 3 },
+  { name: "city", label: "City", id: 4 },
+];
+
 const UserForm = ({
   setIsModalActive,
   addUser,
@@ -23,127 +30,88 @@ const UserForm = ({
 }) => {
   const { img = null, ...restUserParams } = userToEdit || {};
   const [image, setImage] = React.useState(img);
-  const formik = useFormik({
-    initialValues: restUserParams || defaultValue,
-    validationSchema: USER_VALIDATION,
 
-    onSubmit: (values) => {
-      if (userToEdit) {
-        addUser({
-          ...values,
-          id: userToEdit.id,
-          rating: userToEdit.rating,
-          img: image,
-        });
-        return;
-      }
-      const userRating = tableLenght + 1;
-      let userId = Date.now().toString();
-      addUser({ ...values, id: userId, rating: userRating, img: image });
-    },
-  });
+  const handleSubmit = (values) => {
+    if (userToEdit) {
+      addUser({
+        ...values,
+        id: userToEdit.id,
+        rating: userToEdit.rating,
+        img: image,
+      });
+      return;
+    }
+    const userRating = tableLenght + 1;
+    let userId = Date.now().toString();
+    addUser({ ...values, id: userId, rating: userRating, img: image });
+  };
 
   return (
     <div className={styled.modalContainer}>
       <div className={styled.modal}>
-        <form onSubmit={formik.handleSubmit}>
-          <div className={styled.uploadWrapper}>
-            {image ? (
-              <Avatar
-                sx={{ width: 100, height: 100 }}
-                src={URL.createObjectURL(image)}
-              />
-            ) : (
-              <Avatar sx={{ width: 100, height: 100 }} />
-            )}
-            <MyUpload
-              name="img"
-              onChange={(event) => {
-                setImage(event.target.files[0]);
-              }}
-              value={formik.values.img}
-              accept={"image/*"}
-            >
-              {image ? "Изменить картинку" : "Загрузить картинку"}
-            </MyUpload>
-          </div>
-          <div className={styled.formGroup}>
-            <TextField
-              error={
-                formik.touched.firstName && formik.errors.firstName
-                  ? true
-                  : false
-              }
-              label="FirstName"
-              variant="outlined"
-              name="firstName"
-              onChange={formik.handleChange}
-              value={formik.values.firstName}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.firstName && formik.errors.firstName ? (
-              <p style={{ color: "red" }}>{formik.errors.firstName}</p>
-            ) : null}
-          </div>
-          <div className={styled.formGroup}>
-            <TextField
-              error={
-                formik.touched.lastName && formik.errors.lastName ? true : false
-              }
-              label="LastName"
-              variant="outlined"
-              name="lastName"
-              onChange={formik.handleChange}
-              value={formik.values.lastName}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.lastName && formik.errors.lastName ? (
-              <p style={{ color: "red" }}>{formik.errors.lastName}</p>
-            ) : null}
-          </div>
-          <div className={styled.formGroup}>
-            <TextField
-              error={formik.touched.age && formik.errors.age ? true : false}
-              label="Age"
-              variant="outlined"
-              name="age"
-              onChange={formik.handleChange}
-              value={formik.values.age}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.age && formik.errors.age ? (
-              <p style={{ color: "red" }}>{formik.errors.age}</p>
-            ) : null}
-          </div>
-          <div className={styled.formGroup}>
-            <TextField
-              error={formik.touched.city && formik.errors.city ? true : false}
-              label="City"
-              variant="outlined"
-              name="city"
-              onChange={formik.handleChange}
-              value={formik.values.city}
-              onBlur={formik.handleBlur}
-            />
-            {formik.touched.city && formik.errors.city ? (
-              <p style={{ color: "red" }}>{formik.errors.city}</p>
-            ) : null}
-          </div>
-          <div className={styled.btnGroup}>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setIsModalActive(false);
-                setUserToEdit(null);
-              }}
-            >
-              Cancel
-            </Button>
-            <Button type="submit" variant="contained">
-              Submit
-            </Button>
-          </div>
-        </form>
+        <Formik
+          initialValues={restUserParams || defaultValue}
+          validationSchema={USER_VALIDATION}
+          onSubmit={handleSubmit}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <div className={styled.uploadWrapper}>
+                {image ? (
+                  <Avatar
+                    sx={{ width: 100, height: 100 }}
+                    src={URL.createObjectURL(image)}
+                  />
+                ) : (
+                  <Avatar sx={{ width: 100, height: 100 }} />
+                )}
+                <MyUpload
+                  name="img"
+                  onChange={(event) => {
+                    setImage(event.target.files[0]);
+                  }}
+                  accept={"image/*"}
+                >
+                  {image ? "Изменить картинку" : "Загрузить картинку"}
+                </MyUpload>
+              </div>
+              {userInfo.map((item) => {
+                return (
+                  <div className={styled.formGroup} key={item.id}>
+                    <Field
+                      as={TextField}
+                      label={item.label}
+                      variant="outlined"
+                      name={item.name}
+                      error={
+                        errors[item.name] && touched[item.name] ? true : false
+                      }
+                    />
+                    {errors[item.name] && touched[item.name] ? (
+                      <p className={styled.validationError}>
+                        {errors[item.name]}
+                      </p>
+                    ) : null}
+                  </div>
+                );
+              })}
+              <div className={styled.btnGroup}>
+                <Button
+                  variant="outlined"
+                  onClick={() => {
+                    setIsModalActive(false);
+                    setUserToEdit(null);
+                  }}
+                >
+                  Cancel
+                </Button>
+                <Button type="submit" variant="contained">
+                  Submit
+                </Button>
+              </div>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
--- src/components/Users/UserValidation.js
+++ src/components/Users/UserValidation.js
@@ -1,6 +1,6 @@
 import * as Yup from "yup";
 
-export const USER_VALIDATION = Yup.object({
+export const USER_VALIDATION = Yup.object().shape({
   firstName: Yup.string()
     .max(15, "Must be 15 characters or less")
     .required("Required"),
